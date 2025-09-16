7a016a4573b366fdbac96aeaf01663865f3fd2a4
JexieAshleyCapuno-UCM <capunojexieashley27@gmail.com>
2025-09-16 08:52:11 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,29 @@
+public class Comments {
+
+    // This is a single-line comment
+    
+    /*
+     * This is a multi-line comment.
+     * It can span multiple lines.
+     */
+    
+    /**
+     * This is a JavaDoc comment.
+     * It is used to describe classes, methods, and fields.
+     */
+    public static void main(String[] args) {
+        
+        // Single-line comment: This is the main method.
+        
+        /*
+         * Multi-line comment:
+         * No code will be executed here.
+         * Only comments are present in this example.
+         */
+        
+        /**
+         * JavaDoc comment:
+         * This main method does not perform any actions.
+         */
+    }
+}
---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,9 @@
+public class FavoriteSong {
+
+    public static void main(String[] args) {
+        System.out.println("No one even knows how hard life was");
+        System.out.println("I don't even think about it now because");
+        System.out.println("I finally found you");
+        System.out.println("Now my life is sweet like cinnamon");
+    }
+}
